10fc511e7aabe0360f3939ce720b361a60783de4
rajenderbeeram <rajenderbeeram@gmail.com>
2025-01-18 13:33:37 +0530

    from local eclpise

--- src/testingsetup.java
+++ src/testingsetup.java
@@ -16,12 +16,12 @@ try {
         driver.get("https://www.flipkart.com/");
          WebElement s1= driver.findElement(By.xpath("//A[@title='Login']"));
          String actualText = s1.getText();
-         String expectedText = "Login test dfg";
+         String expectedText = "Login";
          
         // Print the title
         System.out.println("Page title is : " + driver.getTitle());
         if(actualText.equals(expectedText)) {
-        	System.out.println("Login present not");
+        	System.out.println("Login code present not");
         	
         }
         else {
