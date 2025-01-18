c19826c219b42f15e0273361e6792f948e9bcda0
rajenderhighlevel <115452000+rajenderhighlevel@users.noreply.github.com>
2025-01-18 12:30:46 +0530

    initial commit with first test case

---
+++ src/testingsetup.java
@@ -0,0 +1,41 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class testingsetup {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        // Initialize the WebDriver
+        WebDriver driver = new ChromeDriver();
+try {
+        // Open Google
+        driver.get("https://www.flipkart.com/");
+         WebElement s1= driver.findElement(By.xpath("//A[@title='Login']"));
+         String actualText = s1.getText();
+         String expectedText = "Login";
+         
+        // Print the title
+        System.out.println("Page title is: " + driver.getTitle());
+        if(actualText.equals(expectedText)) {
+        	System.out.println("Login present");
+        	
+        }
+        else {
+        	System.out.println("Not present Login");
+        }
+    }
+catch(Exception e) {
+	System.out.println("error message is "+ e.getMessage());
+	
+}
+finally{
+	 // Close the browser
+    driver.quit();	
+}
+       
+    }
+}
